2fdaadd2c5a7dc1a12a8f8e8c3acb4e042afeb38
Kaya Dağlı <kayadagli27@gmail.com>
2021-04-05 16:18:49 +0300

    TileVania

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+
+    [SerializeField] AudioClip coinPickUpSFX;
+    [SerializeField] int pointsForCoinPickup = 100;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var playerBody = FindObjectOfType<PlayerScript>().GetComponent<CapsuleCollider2D>();
+        if (collision == playerBody)
+        {
+            FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
+            AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position, 0.3f);
+            Destroy(gameObject);
+        }
+
+    }
+
+
+}
